35b38216cac228bac060ddf081cfd91b9efb4fe2
FulconisYan <yan.fulconis@gmail.com>
2018-11-16 11:01:51 +0100

    Mouvement primaire

--- js/canvas.js
+++ js/canvas.js
@@ -1,6 +1,11 @@
 window.onload = init;
 
-let canvas, ctx, w, h;
+let canvas, ctx, w, h,incrementX,incrementY;
+incrementX = 0;
+incrementY = 0;
+let x=20;
+let y =20;
+let vitesseMarche=10;
 
 let player = new Image();
 player.src = "images/characters/player.png"
@@ -13,9 +18,52 @@ function init() {
   
   w = canvas.width;
   h = canvas.height;
+
+  window.addEventListener('keydown',handleKeydown,false);
+  window.addEventListener('keyup',handleKeyup,false);
   
-  ctx.drawImage(player, 20, 20);
+  ctx.drawImage(player, x, y);
     
   // on demarre la boucle d'animation
   requestAnimationFrame(mainloop); //Créer la fonction
+}
+
+function handleKeydown(evt)
+{
+	switch(evt.key)
+	{
+		case "ArrowLeft":
+			incrementY = 0;
+			incrementX = -vitesseMarche;
+		break;
+		case "ArrowRight":
+			incrementY = 0;
+			incrementX = vitesseMarche;
+		break;
+		case "ArrowUp":
+			incrementY = -vitesseMarche;
+			incrementX = 0;
+		break;
+		case "ArrowDown":
+			incrementY = vitesseMarche;
+			incrementX = 0;
+		break;
+	}
+}
+function handleKeyup(evt)
+{
+	incrementX = 0;
+	incrementY = 0;
+}
+
+function mainloop() {
+	// On efface le canvas
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+	// Dessiner
+	ctx.drawImage(player,x,y);
+	// Bouger
+	x+= incrementX;
+	y+= incrementY;
+	// 
+	requestAnimationFrame(mainloop);
 }
